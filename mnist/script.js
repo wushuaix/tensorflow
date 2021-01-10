dba6a345441fb3194db9ebe1bf935af0001b9775
wushuai <1950882491@qq.com>
2021-01-11 01:56:26 +0800

    '1'

--- mnist/script.js
+++ mnist/script.js
@@ -5,7 +5,7 @@ import { MnistData } from './data';
 window.onload = async () => {
     const data = new MnistData();
     await data.load();
-    const examples = data.nextTestBatch(1000);
+    const examples = data.nextTestBatch(20);
     const surface = tfvis.visor().surface({ name: '输入示例' });
     // 将数据转化为图片
     for (let i = 0; i < 20; i += 1) {
@@ -23,20 +23,34 @@ window.onload = async () => {
         await tf.browser.toPixels(imageTensor, canvas);
         surface.drawArea.appendChild(canvas);
     }
-
+    /* 卷积层: 使用多个卷积核对图像进行卷积操作提取特征
+        卷积层有权重需要训练, 卷积核就是权重
+        池化层: 提取最强体征, 扩大感受野, 减少计算量, 池化层没有权重需要训练
+        全连接层: 作为输出层, 作为分类器, 有权重需要训练
+    */
     const model = tf.sequential();
+    // 第一层特征提取:
+    // 卷积层
     model.add(tf.layers.conv2d({
+        // 图片的形状
         inputShape: [28, 28, 1],
+        // 卷积核大小, 常设置为奇数, 有中间点
         kernelSize: 5,
+        // 特征提取数量
         filters: 8,
+        // 移动步长
         strides: 1,
+        // 激活函数, relu移除掉不常用的特征
         activation: 'relu',
+        // 卷积核初始化方法
         kernelInitializer: 'varianceScaling'
     }));
+    // 最大池化层
     model.add(tf.layers.maxPool2d({
         poolSize: [2, 2],
         strides: [2, 2]
     }));
+    // 第二轮特征提取:
     model.add(tf.layers.conv2d({
         kernelSize: 5,
         filters: 16,
@@ -48,22 +62,27 @@ window.onload = async () => {
         poolSize: [2, 2],
         strides: [2, 2]
     }));
+    // 将高维的filter提取的特征图转化为一维
     model.add(tf.layers.flatten());
+    // 全连接层
     model.add(tf.layers.dense({
         units: 10,
         activation: 'softmax',
         kernelInitializer: 'varianceScaling'
     }));
     model.compile({
+        // 交叉熵损失函数
         loss: 'categoricalCrossentropy',
+        // 设置优化器
         optimizer: tf.train.adam(),
+        // 设置准确度
         metrics: ['accuracy']
     });
-
+    // 设置训练集和验证集
     const [trainXs, trainYs] = tf.tidy(() => {
-        const d = data.nextTrainBatch(1000);
+        const d = data.nextTrainBatch(2000);
         return [
-            d.xs.reshape([1000, 28, 28, 1]),
+            d.xs.reshape([2000, 28, 28, 1]),
             d.labels
         ];
     });
@@ -78,7 +97,6 @@ window.onload = async () => {
 
     await model.fit(trainXs, trainYs, {
         validationData: [testXs, testYs],
-        batchSize: 500,
         epochs: 20,
         callbacks: tfvis.show.fitCallbacks(
             { name: '训练效果' },
